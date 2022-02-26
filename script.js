cf7418455023973147e7c6f8d87bb80ae4306118
Ravnish Gupta <ravnish@gmail.com>
2022-02-25 21:37:21 -0800

    Javascript updated

---
+++ script.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+ var findMedianSortedArrays = function(nums1, nums2) {
+    //add elements of the second array to the first array
+    //sort the array
+    //if length is an odd number, divide the length by 2 and get the ceiling value
+    //get the element at that value
+    //if length is an even number, divide the length by 2 and get the element the length/2 and length/2 + 1, add them and divide by 2
+    
+    for (var i=0; i<nums2.length; i++) {
+        nums1.push(nums2[i])
+    }
+    
+    nums1.sort(function(a, b) {
+      return a - b;
+    });
+    
+    var tempVal = (nums1.length) % 2;
+    var tempVal1 = nums1.length
+    
+    if (tempVal === 0) { //even {
+        return (nums1[tempVal1/2] + nums1[tempVal1/2-1])/2;
+    }
+    else //odd
+    {
+      return nums1[Math.floor(tempVal1/2)];   
+    }
+ };
